0940c00d409197ce6d8351217722e6cb4b7edd7b
Takashi Kitajima <inc@2inc.org>
2019-02-04 09:53:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Snow Monkey
+
+子テーマの `functions.php` にカスタマイズコードを追加するように、このプラグインの `my-snow-monkey.php` に書くと、同じようにカスタマイズが反映されます。
---
+++ my-snow-monkey.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin name: My Snow Monkey
+ * Description: このプラグインに、あなたの Snow Monkey 用カスタマイズコードを書いてください。
+ * Version: 0.1.0
+ *
+ * @package my-snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+/**
+ * Snow Monkey 以外のテーマを利用している場合は有効化してもカスタマイズが反映されないようにする
+ */
+$theme = wp_get_theme();
+if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->template ) {
+ return;
+}
