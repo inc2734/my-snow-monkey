9940cdcc8abe45a78bdad0e2c0474ea5ad6448f2
Takashi Kitajima <inc@2inc.org>
2019-08-20 21:45:32 +0900

    some fixes

--- my-snow-monkey.php
+++ my-snow-monkey.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: My Snow Monkey
  * Description: このプラグインに、あなたの Snow Monkey 用カスタマイズコードを書いてください。
- * Version: 0.1.0
+ * Version: 0.1.1
  *
  * @package my-snow-monkey
  * @author inc2734
@@ -12,7 +12,7 @@
 /**
  * Snow Monkey 以外のテーマを利用している場合は有効化してもカスタマイズが反映されないようにする
  */
-$theme = wp_get_theme();
+$theme = wp_get_theme( get_template() );
 if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->template ) {
 	return;
 }
