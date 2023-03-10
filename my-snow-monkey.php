09d1dc0e186e80cef974f327946093e97774474a
inc2734 <inc@2inc.org>
2023-03-10 15:17:23 +0900

    Some updates

--- my-snow-monkey.php
+++ my-snow-monkey.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: My Snow Monkey
  * Description: このプラグインに、あなたの Snow Monkey 用カスタマイズコードを書いてください。
- * Version: 0.2.1
+ * Version: 0.2.2
  *
  * @package my-snow-monkey
  * @author inc2734
